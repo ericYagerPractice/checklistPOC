b0c62c2c2c2ce6d8c90f3a7d2425063f9092687c
ericYager <eyager@amazon.com>
2021-08-28 18:50:10 -0500

    removed mdbootstrap because it provides a frustrating error. Implemented semantic UI react with a canned page. Committing incomplete implementation to validate production build.

---
+++ src/Routes.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Home from './pages/Home'
+
+class Routes extends React.Component {
+  render() {
+    return (
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route
+          render={function() {
+            return <h1>Not Found</h1>;
+          }}
+        />
+      </Switch>
+    );
+  } 
+}
+
+export default Routes;
